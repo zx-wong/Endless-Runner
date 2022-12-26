9d3b1c39295e1be3d40c9f483579c9d78762c147
Zx <whizzzx_@hotmail.com>
2022-12-26 15:01:17 +0800

    Endless Runner to Github

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    private Vector2 targetPos;
+
+    public float Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+
+    public GameObject effect;
+    public GameObject gameOver;
+    public GameObject scoreManger;
+    public Text healthDisplay;
+
+
+    // Update is called once per frame
+    private void Update ()
+    {
+        healthDisplay.text = health.ToString();
+
+        if (health <= 0)
+        {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+            scoreManger.SetActive(false);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            Instantiate(effect, transform.position, Quaternion.identity);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            Instantiate(effect, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int score;
+    public Text scoreDisplay;
+
+    private void Update()
+    {
+        scoreDisplay.text = score.ToString();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            score++;
+            Debug.Log(score);
+        }
+    }
+
+}
